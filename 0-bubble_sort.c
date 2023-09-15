36f34445f2b5065b462256a0343d6df8c5c1a401
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-09-14 22:40:44 -0400

    bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+
+#include "sort.h"
+/**
+ * bubble_sort - Sort an array using bubblesort algorithm
+ * @array: Array to sort
+ * @size: Size of array
+ * Return: Nothing, just sort and print when swap
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int tmp;
+
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < (size - i - 1); j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
+}
