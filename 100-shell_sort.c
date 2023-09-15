b4c6f4d0afcddfd557a4ede1a5e9bcc2d661cc63
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-09-14 23:22:45 -0400

    knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+    size_t gap, i, j;
+    int tmp;
+
+    if (array == NULL || size < 2)
+        return;
+
+    gap = 1;
+    while (gap < size)
+        gap = gap * 3 + 1;
+    gap = (gap - 1) / 3;
+    while (gap > 0)
+    {
+        for (i = gap; i < size; i++)
+        {
+            tmp = array[i];
+            for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+                array[j] = array[j - gap];
+            array[j] = tmp;
+        }
+        gap = (gap - 1) / 3;
+        print_array(array, size);
+    }
+}
+
+
+
