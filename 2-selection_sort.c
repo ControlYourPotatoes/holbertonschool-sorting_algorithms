aaade5c68a0d1561085d7141dca62199f2b928b2
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-09-14 22:56:09 -0400

    betty again

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,41 +1,33 @@
+
 #include "sort.h"
-/**
- * _swap - swap two numbers
- * @a: integer
- * @b: integer
- **/
-void _swap(int *a, int *b)
-{
-	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
 /**
- * selection_sort - sort array using selection sort algorithm
- * @array: array
- * @size: array size
- **/
-
+ * selection_sort - Sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, min;
+	size_t i, j, min;
+	int tmp;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
+		for (j = (i + 1); j < size; j++)
 		{
-			if (array[min] > array[j])
+			if (array[j] < array[min])
 				min = j;
 		}
 		if (min != i)
 		{
-			_swap(&array[i], &array[min]);
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
 			print_array(array, size);
 		}
 	}
