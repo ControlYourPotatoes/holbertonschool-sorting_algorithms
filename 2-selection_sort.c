7d38ccabfac0f953b589454b7ea3839149a3c283
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-09-14 22:51:37 -0400

    sort selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,43 @@
-
 #include "sort.h"
-
 /**
- * selection_sort - Sorts an array of integers in ascending order
- * using the Selection sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
- */
+ * _swap - swap two numbers
+ * @a: integer
+ * @b: integer
+ **/
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * selection_sort - sort array using selection sort algorithm
+ * @array: array
+ * @size: array size
+ **/
+
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int tmp;
+	unsigned int i, j, min;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = (i + 1); j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
-        if (min != i)
-        {
-            tmp = array[i];
-            array[i] = array[min];
-            array[min] = tmp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			_swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
+
 }
